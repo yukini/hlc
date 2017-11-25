a07a163dc04aacef31a43edf697d8b11e0c9d517
yukini <yumiko.mukai1003@gmail.com>
2017-11-26 01:41:11 +0900

    managed attribute.

--- entry/models.py
+++ entry/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class UserInfo(models.Model):
@@ -17,3 +18,22 @@ class UserInfo(models.Model):
     tel = models.CharField(max_length=20)
 
     pub_date = models.DateTimeField('date published')
+
+    def post(self):
+        self.pub_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.pub_date
+
+    # マネージドアトリビュー ト (managed attribute)
+    # http://qh73xebitbucketorg.readthedocs.io/ja/latest/1.Programmings/python/LIB/django/model/main/#id4
+    def _get_full_kanji_name(self):
+        "Returns the person's full name."
+        return '%s %s' % (self.kanji_name_last, self.kanji_name_first)
+    full_kanji_name = property(_get_full_kanji_name)
+
+    def _get_full_kana_name(self):
+        "Returns the person's full name."
+        return '%s %s' % (self.kana_name_last, self.kana_name_first)
+    full_kana_name = property(_get_full_kana_name)
